0311686f36549865827b62fd2ebb69fd90275390
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-09-06 15:23:14 +0100

    Add aws public IP

--- scripts/create_client_topics.sh
+++ scripts/create_client_topics.sh
@@ -3,7 +3,6 @@
 
 set -e
 
-
 clients=($(find creds/clients -maxdepth 1 -mindepth 1 -type d -printf '%f\n'))
 
 for client in "${clients[@]}"; do
@@ -13,28 +12,29 @@ for client in "${clients[@]}"; do
 
     # Create tutorials topic
     kafka-topics --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server localhost:19093 \
+        --bootstrap-server ec2-13-50-5-220.eu-north-1.compute.amazonaws.com:19093 \
         --create \
-        --topic "$client"
+        --topic "$client" \
+        --partitions 16
 
     # Add write permissions to client
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server localhost:19093 \
+        --bootstrap-server ec2-13-50-5-220.eu-north-1.compute.amazonaws.com:19093 \
         --add --allow-principal "User:${client}" \
         --operation write \
         --topic "$client"
 
     # Add read permissions for topic
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server localhost:19093 \
+        --bootstrap-server ec2-13-50-5-220.eu-north-1.compute.amazonaws.com:19093 \
         --add \
         --allow-principal "User:${client}" \
         --operation read \
         --topic "$client"
-    
+
     # Add read permissions for all groups
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server localhost:19093 \
+        --bootstrap-server ec2-13-50-5-220.eu-north-1.compute.amazonaws.com:19093 \
         --add \
         --allow-principal "User:${client}" \
         --operation read \
