e309babbf807e6f78425be94a02c1f31c4e29374
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-10-30 08:24:44 +0100

    Parameterize kafka cluster topic creation

--- scripts/create_group_topics.sh
+++ scripts/create_group_topics.sh
@@ -4,6 +4,7 @@
 set -e
 
 clients=($(find creds/groups -maxdepth 1 -mindepth 1 -type d -printf '%f\n'))
+CLUSTER_ENDPOINT=13.60.146.188:19093
 
 for client in "${clients[@]}"; do
     echo
@@ -12,21 +13,21 @@ for client in "${clients[@]}"; do
 
     # Create tutorials topic
     kafka-topics --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server 13.49.128.80:19093 \
+        --bootstrap-server "$CLUSTER_ENDPOINT" \
         --create \
         --topic "$client" \
         --partitions 16
 
     # Add write permissions to client
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server 13.49.128.80:19093 \
+        --bootstrap-server "$CLUSTER_ENDPOINT" \
         --add --allow-principal "User:${client}" \
         --operation write \
         --topic "$client"
 
     # Add read permissions for topic
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server 13.49.128.80:19093 \
+        --bootstrap-server "$CLUSTER_ENDPOINT" \
         --add \
         --allow-principal "User:${client}" \
         --operation read \
@@ -34,7 +35,7 @@ for client in "${clients[@]}"; do
 
     # Add read permissions for all groups
     kafka-acls --command-config ./creds/admins/landau/client-ssl.properties \
-        --bootstrap-server 13.49.128.80:19093 \
+        --bootstrap-server "$CLUSTER_ENDPOINT" \
         --add \
         --allow-principal "User:${client}" \
         --operation read \
